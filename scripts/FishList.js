a552d8eedc3c978e8f288b3317fc0243c5586f68
Curtis Webb <curtwebb1194@@gmail.com>
2023-02-24 23:23:23 -0600

    My first version of my aquarium code

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -7,24 +7,24 @@ export const fishList = () => {
 
 
     // Start building a string filled with HTML syntax
-   let htmlString = '<article class="fishList">'
+    let htmlString = '<article class="fishList">' //This variable is the name of the string we are looping.
 
 
     // Create HTNL representations of each fish here
-    for (const fish of fishes) {
+    for (const fish of fishes) {    //we are iterating through our getfish function from the database.js that we imported at the top
 
-        // Why is there a backtick used for this string?
-        htmlString += `<section class="tank">
-            <img class ="fish_image" src="${fish.image}">
+        // Why is there a backtick used for this string? we back tick easiily put object properties in HTML format in Javascript
+        htmlString += `<section class="tank"> 
+            <img class ="fish_image" src="${fish.image}"> 
             <div class="fish_name">This is ${fish.name}!</div>
             <div class="fish_species">I'm a ${fish.species}.</div>
             <div class="fish_length">I'm a level ${fish.size} fish.</div>
             <div class="fish_location">You can find me in ${fish.location}.</div>
             <div class="fish_diet">I eat ${fish.food}.</div>
         </section>`
-
+    //REMEMBER** Note Img class and src.
     }
-    htmlString += `</article>`
+    htmlString += `</article>` //the operator += add two strings together** add one at the start too.
 
     return htmlString
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 import { getFish } from './database.js' //Note: The imported function is wrapped in curly braces. This is needed for anything that is exported from a module that isn't the default export.
-import { fishList } from './FishList.js'
+import { fishList } from './FishList.js' //brings in the function that makes everything happen!
 const allFish = getFish()
 
 for (const fish of allFish) {
     console.log(fish)
 }
 
-const parentHTMLElement = document.querySelector(".fishList")
+const parentHTMLElement = document.querySelector(".fishList") //document.query selector grabs fishlist function (which is the selector)
 
-parentHTMLElement.innerHTML = fishList()
+parentHTMLElement.innerHTML = fishList() //gets the HTML Markup within the element listed
